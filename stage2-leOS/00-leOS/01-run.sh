ed71c227a0611aa5559ffc153c21faffbd4077ca
Edward Wang <edawordwang@gmail.com>
2026-06-23 15:39:28 -0400

    git rebase, enable SPI

--- stage2-leOS/00-leOS/01-run.sh
+++ stage2-leOS/00-leOS/01-run.sh
@@ -4,4 +4,5 @@ cd /opt
 curl -fL https://ziglang.org/download/0.15.1/zig-aarch64-linux-0.15.1.tar.xz | tar xJ
 ln -sf /opt/zig-aarch64-linux-0.15.1/zig /usr/local/bin/zig
 systemctl enable ssh
+git config --system pull.rebase true
 EOF
