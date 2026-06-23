089eb9068204adefc429d37d4992ce8489618d85
Edward Wang <edawordwang@gmail.com>
2026-06-23 15:13:25 -0400

    curl -f, groups in user-data, enable ssh

--- stage2-leOS/00-leOS/01-run.sh
+++ stage2-leOS/00-leOS/01-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 on_chroot << EOF
 cd /opt
-curl -L https://ziglang.org/download/0.15.1/zig-aarch64-linux-0.15.1.tar.xz | tar xJ
+curl -fL https://ziglang.org/download/0.15.1/zig-aarch64-linux-0.15.1.tar.xz | tar xJ
 ln -sf /opt/zig-aarch64-linux-0.15.1/zig /usr/local/bin/zig
-usermod -aG video,render,input "${FIRST_USER_NAME}"
+systemctl enable ssh
 EOF
